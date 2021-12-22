6f3cdcebd951a1bf795fafacd11a7882d1d0cfb3
Radek Jerzynski <47062194+rdx7777@users.noreply.github.com>
2021-12-22 12:21:38 +0000

    Initial commit.

---
+++ src/test/java/com/github/rdx7777/userproduct/UserProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rdx7777.userproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
